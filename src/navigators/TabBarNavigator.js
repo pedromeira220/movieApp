360ac8d439b74eab282a5f91c58ad6fb72fdf29e
Pedro Meira <petergamer8k@gmail.com>
2022-06-29 21:00:15 -0300

    Removing the buttom on the tab bar that goes to the profile screen

--- src/navigators/TabBarNavigator.js
+++ src/navigators/TabBarNavigator.js
@@ -14,68 +14,59 @@ import { View } from 'react-native';
 const Tab = createBottomTabNavigator();
 export function TabBarNavigator() {
     return (
-    
-            <Tab.Navigator
-                screenOptions={{
-                    headerShown: false,
-                    tabBarStyle: {
-                        backgroundColor: theme.colors.background,
-                        borderTopColor: "transparent",
+
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: theme.colors.background,
+                    borderTopColor: "transparent",
 
 
-                    },
-                    tabBarActiveTintColor: theme.colors.primary,
-                    tabBarInactiveTintColor: theme.colors.inactiveTabBar,
-                    tabBarShowLabel: false,
+                },
+                tabBarActiveTintColor: theme.colors.primary,
+                tabBarInactiveTintColor: theme.colors.inactiveTabBar,
+                tabBarShowLabel: false,
 
+            }}
+
+
+        >
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ size, color, focused }) => (
+                        <View
+                            style={{
+                                flex: 1,
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }}
+                        >
+                            <Foundation name="home" size={size * 1.4} color={color}
+                                resizeMode="cover"
+                            />
+                        </View>
+                    )
                 }}
+            />
+            <Tab.Screen
+                name="DiscoveryScreen"
+                component={DiscoveryScreen}
+                options={{
+                    tabBarIcon: ({ size, color, focused }) => (
+                        <FontAwesome name="search" size={size * 1.4} color={color} />
+
+                    )
+                }}
+            />
 
 
-            >
-                <Tab.Screen
-                    name="Home"
-                    component={Home}
-                    options={{
-                        tabBarIcon: ({ size, color, focused }) => (
-                            <View
-                                style={{
-                                    flex: 1,
-                                    justifyContent: "center",
-                                    alignItems: "center"
-                                }}
-                            >
-                                <Foundation name="home" size={size * 1.4} color={color}
-                                    resizeMode="cover"
-                                />
-                            </View>
-                        )
-                    }}
-                />
-                <Tab.Screen
-                    name="DiscoveryScreen"
-                    component={DiscoveryScreen}
-                    options={{
-                        tabBarIcon: ({ size, color, focused }) => (
-                            <FontAwesome name="play-circle" size={size} color={color} />
 
-                        )
-                    }}
-                />
-                <Tab.Screen
-                    name="ProfileScreen"
-                    component={ProfileScreen}
-                    options={{
-                        tabBarIcon: ({ size, color, focused }) => (
-                            <FontAwesome name="user" size={size * 1.4} color={color} />
+        </Tab.Navigator>
 
-                        )
-                    }}
-                />
-                
 
-            </Tab.Navigator>
-
-        
 
     )
 }
